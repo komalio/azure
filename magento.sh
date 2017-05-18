6bc234fb2dd1aa20ab8af16c771d38c3026cfa7c
komalio <komalio@sysgain.com>
2017-05-18 12:16:01 +0530

    Add files via upload

---
+++ magento.sh
@@ -0,0 +1,11 @@
+sudo sed -i "s/127.0.0.1/\*/" "/etc/mysql/mysql.conf.d/mysqld.cnf"
+service mysql restart
+sleep 15
+
+# create random password
+DBUSER=$1
+PASSWDDB=$2
+# replace "-" with "_" for database username
+
+#mysql -uroot -pSysgain@12345 -e "CREATE DATABASE ${DBUSER} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+mysql -uroot -pSysgain@12345 -e "CREATE USER ${DBUSER} IDENTIFIED BY '${PASSWDDB}';"
