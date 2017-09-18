39b6345a4ee7521da8d80e60bbd0fa29f06dedf6
komali <komalio@sysgain.com>
2017-09-19 00:12:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysql@123'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysql@123'
-sudo apt-get -y install mysql-server mysql-client
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysql@123'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysql@123'
+apt-get -y install mysql-server mysql-client
