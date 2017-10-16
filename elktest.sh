dde14a3a15e1e6cb1260c12341e53b10e026b459
komali <komalio@sysgain.com>
2017-10-16 23:57:55 +0530

    Update elktest.sh

--- elktest.sh
+++ elktest.sh
@@ -5,7 +5,7 @@
 DATE=`date +%Y%m%d%T`
 LOG=/tmp/elkstack_deploy.log.$DATE
 HOSTIP=`hostname -i`
-storageAccount=$1
+
 
 
 # Configure Repos for Java, Elasticsearch, Kibana Packages
@@ -49,7 +49,7 @@ sudo systemctl start kibana >> $LOG
 #Configuring Nginx
 echo "---Configuring Nginx---" >> $LOG
 sudo sudo -v >> $LOG
-echo "admin:`openssl passwd -apr1 'Password4321'`" | sudo tee -a /etc/nginx/htpasswd.users >> $LOG
+echo "admin:`openssl passwd -apr1 'Password@!234'`" | sudo tee -a /etc/nginx/htpasswd.users >> $LOG
 cat /dev/null > /etc/nginx/sites-available/default >> $LOG
 wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/default -O /etc/nginx/sites-available/default >> $LOG
 sudo nginx -t >> $LOG
@@ -66,10 +66,10 @@ sudo openssl req -config /etc/ssl/openssl.cnf -x509 -days 3650 -batch -nodes -ne
 
 #Configuring Logstash
 echo "---Configuring Logstash---" >> $LOG
-wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/02-beats-input.conf -O /etc/logstash/conf.d/02-beats-input.conf >> $LOG
+sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/02-beats-input.conf -O /etc/logstash/conf.d/02-beats-input.conf >> $LOG
 sudo ufw allow 5044 >> $LOG
-wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/10-syslog-filter.conf -O /etc/logstash/conf.d/10-syslog-filter.conf >> $LOG
-wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/30-elasticsearch-output.conf -O /etc/logstash/conf.d/30-elasticsearch-output.conf >> $LOG
+sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/10-syslog-filter.conf -O /etc/logstash/conf.d/10-syslog-filter.conf >> $LOG
+sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/30-elasticsearch-output.conf -O /etc/logstash/conf.d/30-elasticsearch-output.conf >> $LOG
 sudo /opt/logstash/bin/logstash --configtest -f /etc/logstash/conf.d/ >> $LOG
 sudo systemctl restart logstash >> $LOG
 sudo systemctl enable logstash >> $LOG
