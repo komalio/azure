3905e9e65935d540420439bdffb15dded8b0f056
komali <komalio@sysgain.com>
2018-01-03 21:27:30 +0530

    Create dvwa.sh

---
+++ dvwa.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+wget https://github.com/komalio/komali-test/raw/master/DVWA-master.zip
+unzip DVWA-master.zip
+sudo mv DVWA-master /var/www/html/dvwa
+sudo chmod -R 755 /var/www/html/dvwa
+sudo cp /var/www/html/dvwa/config/config.inc.php.dist /var/www/html/dvwa/config/config.inc.php
+sudo apt-get update
+sudo apt-get install php-gd -y
+sudo service apache2 start
+sudo service mysql start
+sudo mysql -u root -e "create database dvwa";
+sudo mysql -u root -e "create user DVWA";
+sudo mysql -u root -e "grant all on dvwa.* to DVWA@localhost IDENTIFIED BY 'password'";
+sudo mysql -u root -e "FLUSH PRIVILEGES";
+sudo sed -i -e "s/allow_url_include = Off/allow_url_include = On/g" /etc/php/7.0/apache2/php.ini
+sudo sed -i -e "s/root/DVWA/g" /var/www/html/dvwa/config/config.inc.php
+sudo sed -i -e "s/p@ssw0rd/password/g" /var/www/html/dvwa/config/config.inc.php
+sudo sed -i -e "s/public_key' ]  = ''/public_key' ]  = '6LfWsT4UAAAAAJgZBWzGlvN99BZtczIxaoxN5mep'/g" /var/www/html/dvwa/config/config.inc.php
+sudo sed -i -e "s/private_key' ] = ''/private_key' ] = '6LfWsT4UAAAAAFqQN7PtbPMWhjG0-T5fwCytGRPZ'/g" /var/www/html/dvwa/config/config.inc.php
+sudo chgrp www-data /var/www/html/dvwa/hackable/uploads
+sudo chgrp www-data /var/www/html/dvwa/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
+sudo chgrp www-data /var/www/html/dvwa/config
+sudo chmod g+w /var/www/html/dvwa/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt
+sudo chmod g+w /var/www/html/dvwa/hackable/uploads
+sudo chmod g+w /var/www/html/dvwa/config
+sudo service apache2 restart
+sudo service mysql start
