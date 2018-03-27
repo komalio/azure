ffa9bf7265c784ee32dc01264c442ae1febe9a1c
komali <komalio@sysgain.com>
2018-03-27 18:45:01 +0530

    Create datatest.ps1

---
+++ datatest.ps1
@@ -0,0 +1,75 @@
+# https://github.com/sysgain/iot-automation/raw/main/powerBItemplates/PowerBI_Templates.zip
+
+# https://projectiot.blob.core.windows.net/iotp2/PBIDesktop_x64.msi
+
+# https://projectiot.blob.core.windows.net/iothub/LightSensor.msi
+
+# https://projectiot.blob.core.windows.net/iothub/DataServiceSetup.msi
+param(
+    
+    [string] $powerBItemplates = "$1",
+    
+    [string] $powerbidesktop = "$2",
+    
+    [string] $lightsensor = "$3",
+
+    [string] $dataservicesetup = "$4",
+
+    [string] $azureConnection = "$5",
+
+    [string] $azureConnection1 = "$6",
+
+    [string] $azureConnection2 = "$7",
+
+    [string] $storageConnection = "$8",
+
+    [string] $PiServerConnection = "$9",
+
+    [string] $PiServerConnection1 = "$10",
+
+    [string] $PiServerConnection2 = "$11"
+
+    )
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
+    
+    $client = new-object System.Net.WebClient
+    
+    $client.DownloadFile($powerBItemplates,"C:\PowerBI_Templates.zip")
+
+    $client.DownloadFile($lightsensor,"C:\LightSensor.msi")
+
+    C:\LightSensor.msi /qn
+
+    Start-Sleep -s 12
+    
+    $client.DownloadFile($dataservicesetup,"C:\DataServiceSetup.msi")
+
+    C:\DataServiceSetup.msi /qn
+
+    Start-Sleep -s 12
+     
+    $client.DownloadFile($powerbidesktop,"C:\PBIDesktop_x64.msi")
+    
+    $LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+    
+    C:\PBIDesktop_x64.msi /qn /norestart ACCEPT_EULA=1
+
+    $dataserviceconfig = "C:\Program Files (x86)\Default Company Name\DataServiceSetup\DataService.exe.config"
+
+    $doc = (Get-Content $dataserviceconfig) -as [Xml]
+
+    $obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'AzureConnectionString'}
+
+    $obj.value = {$azureConnection+$azureConnection1+$azureConnection2}
+
+    $obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'StorageConnectionString'}
+
+    $obj.value = $storageConnection
+
+    $obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'PiServerConnectionString'}
+
+    $obj.value = {$PiServerConnection+$PiServerConnection1+$PiServerConnection2}
+
+    $doc.Save($dataserviceconfig)
+
+    Start-Service -SERVICENAME DataServiceEM
