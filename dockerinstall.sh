da5a32bb0768f16ec5f973cf3680a4a19e485fd5
komali <komalio@sysgain.com>
2018-12-28 15:14:22 +0530

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -4,7 +4,7 @@
 AZ_REPO=$(lsb_release -cs)
 LOG="/tmp/install.log"
 GIT_URL="$1"
-GIT_PATH="ProjectEdison"
+GIT_PATH="/var/lib/waagent/custom-script/download/0/ProjectEdison"
 git clone $1
 echo "----------script executed successfully-----" >> $LOG
 
@@ -54,7 +54,7 @@ if [ -d $GIT_PATH ]
 then
         echo "------------------------------------" >> $LOG
         echo "The $GIT_PATH exist & clone successful" >> $LOG
-        cd ~/$GIT_PATH
+        cd /$GIT_PATH
         sudo docker-compose build
 
 else
