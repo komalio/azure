0baa575ed117b5258dd8730e7ef8978ff29ac8d3
komali <komalio@sysgain.com>
2019-01-03 14:54:28 +0530

    Update dockerimages.sh

--- scripts/dockerimages.sh
+++ scripts/dockerimages.sh
@@ -59,8 +59,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-kubectl version
- 
+kubectl --help
 if [ $? -eq 0 ]
 then
 echo "------------------------------------" >> $LOG
