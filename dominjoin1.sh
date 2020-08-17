393e7d09ae1cb31bfc1e89c171fde9bb4377cb8a
komali <komalioruganti15@gmail.com>
2020-08-17 20:26:47 +0530

    Create dominjoin1.sh

---
+++ dominjoin1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+hostname="$1"
+domainToJoin="$2"
+adusername="$3"
+adpassword="$4"
+domainUppercase=upper($domainToJoin)
+#Configure the hosts file
+sudo sed -i -e "s/127.0.0.1 localhost/127.0.0.1 $1.$2 $1/g" /etc/hosts
+
+#Install required packages
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -y install krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli 
+
+#Configure NTP
+sudo -i
+sudo echo "server $2" >> /etc/ntp.conf
+sudo systemctl stop ntp
+sudo ntpdate $2
+sudo systemctl start ntp
+
+# Join VM to the managed domain
+sudo realm discover $domainUppercase
+echo $2 | kinit $3
+sudo echo $2 | realm join --verbose $domainUppercase -U $3 --install=/
+
+#Update the SSSD configuration
+sudo sed -i -e "s/use_fully_qualified_names = True/# use_fully_qualified_names = True/g" /etc/sssd/sssd.conf
+sudo systemctl restart sssd
+
+# Allow password authentication for SSH
+#sudo sed -i -e "s/PasswordAuthentication /PasswordAuthentication yes/g" /etc/ssh/sshd_config
+
+#Configure automatic home directory creation
+sudo echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0077" >>/etc/pam.d/common-session
+
+#Grant the 'AAD DC Administrators' group sudo privileges
+sudo echo "# Add 'AAD DC Administrators' group members as admins" >> /etc/sudoers
+sudo echo "%AAD\ DC\ Administrators ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+#signin check
+#ssh -l komali@BORNONTHECLOUD.IN adtestvm.bornonthecloud.in
