9d5883cfdc9dda993033eb959a42d2eadedb1a89
komali <komalioruganti15@gmail.com>
2020-10-29 16:53:20 +0530

    Add files via upload

---
+++ scripts/tempdrivechange.ps1
@@ -0,0 +1,14 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted  -Force
+gwmi win32_pagefilesetting
+$pf=gwmi win32_pagefilesetting
+$pf.Delete()
+Restart-Computer –Force
+
+Get-Partition -DriveLetter "D" | Set-Partition -NewDriveLetter "T"
+$TempDriveLetter = "T"
+$TempDriveLetter = $TempDriveLetter + ":"
+$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
+#re-enable page file on new Drive
+$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
+Set-WMIInstance -Class Win32_PageFileSetting -Arguments @{ Name = "$TempDriveLetter\pagefile.sys"; MaximumSize = 0; }
+Restart-Computer -Force
