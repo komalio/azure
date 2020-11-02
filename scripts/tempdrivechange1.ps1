5775fe234dcaa56dc68821420cc15e28e85f0a65
komali <komalioruganti15@gmail.com>
2020-11-02 11:17:16 +0530

    Update tempdrivechange1.ps1

--- scripts/tempdrivechange1.ps1
+++ scripts/tempdrivechange1.ps1
@@ -6,4 +6,4 @@ $drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveL
 #re-enable page file on new Drive
 $drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
 Set-WMIInstance -Class Win32_PageFileSetting -Arguments @{ Name = "$TempDriveLetter\pagefile.sys"; MaximumSize = 0; }
-Restart-Computer -Force
+#Restart-Computer -Force
